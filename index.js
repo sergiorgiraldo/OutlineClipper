f80599c2741ccd3ad7cac6ec15989d92f64aaaf1
Sergio Giraldo <sergio@giraldo.com.br>
2023-08-23 13:45:07 +0200

    readability works:: by sergio giraldo @ 20230823T1345CEST, gpg signed

--- index.js
+++ index.js
@@ -3,29 +3,61 @@ const form = document.querySelector('.create-clip');
 document.addEventListener('DOMContentLoaded', ()=>{
   chrome.storage.local.get("outlineApiStg", function(item){
     document.getElementById("outline-api-key").value = item.outlineApiStg;
-  });  
-});
+    loadCollections();
+  });
 
-// New clip
+  manager.parseClip();
+});
 
 form.addEventListener('submit', (event)=>{
   event.preventDefault();
   const formData = new FormData(form);
   const data = Object.fromEntries(formData);
 
-  // Extract form values
   const outlineApiKey = data['outline-api-key'];
   const outlineCollection = data['outline-collection'];
   const outlinePage = data['outline-page'];
-  const urlParams = new URLSearchParams(window.location.search);
-  const urlToSave = urlParams.get('u');
 
-  manager.createClip(outlineApiKey, outlineCollection, outlinePage, urlToSave);
+  manager.createClip(outlineApiKey, outlineCollection, outlinePage);
 });
+
+document.getElementById("outline-api-key").addEventListener('change', ()=>{
+  loadCollections();
+});
+
+function loadCollections(){
+  const key = document.getElementById("outline-api-key").value;
+  if (key == "") return;
+
+  fetch("https://husk-dusk.nl/api/collections.list",{
+    method: "POST",
+    headers: {
+      "Authorization": "Bearer " + key,
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    }    
+  }).then(function (response) {
+    let selectElement = document.getElementById("outline-collection");
+
+    while (selectElement.firstChild) {
+      selectElement.removeChild(selectElement.firstChild);
+    }
+
+    response.data.forEach(item => {
+      let optionElement = document.createElement("option");
+      optionElement.value = item.id;
+      optionElement.text = item.name;
+      selectElement.appendChild(optionElement);
+    });    
+  })
+  .catch(function (err) {
+    console.warn('Something went wrong.', err);
+  });
+}
 
 class ClipManager {
   constructor(d) {
-    this.logMessage('Outline clipper: initializing');
+    this.logMessage('Outline clipper: initialized');
   }
 
   logMessage(message) {
@@ -42,9 +74,28 @@ class ClipManager {
     console.log(logLine);
   }
 
-  createClip(outlineApiKey, outlineCollection, outlinePage, urlToSave) {
+  parseClip(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const urlToSave = urlParams.get('u');
+  
+    fetch(urlToSave).then(function (response) {
+      return response.text();
+    })
+    .then(function (html) {
+      var parser = new DOMParser();
+      var doc = parser.parseFromString(html, 'text/html');
+      const reader = new Readability(doc);
+      const article = reader.parse().content;
+      document.getElementById("parsed-clip").innerHTML = article;
+    })
+    .catch(function (err) {
+      console.warn('Something went wrong.', err);
+    });
+  }
+
+  createClip(outlineApiKey, outlineCollection, outlinePage) {
     chrome.storage.local.set({ "outlineApiStg": outlineApiKey }, function(){});
-    this.logMessage(`Created ${outlineApiKey}\n${outlineCollection}\n${outlinePage}\n${urlToSave}`);
+    this.logMessage(`Saved ${outlineCollection}/${outlinePage}`);
   }
 }
 
